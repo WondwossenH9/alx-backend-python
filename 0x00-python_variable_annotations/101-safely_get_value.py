8f73188a7219e4670aa68e15345c829e0d0a53b6
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-07-11 19:40:10 +0000

    fix code

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, TypeVar
 
-def safely_get_value(dct: Dict[Any, Any], key: Any, default: Optional[Any] = None) -> Optional[Any]:
+T = TypeVar('T')
+
+def safely_get_value(dct: Dict[Any, T], key: Any, default: Optional[T] = None) -> Optional[T]:
     """
     Safely retrieves a value from a dictionary, returning a default value if the key is not present.
     """
