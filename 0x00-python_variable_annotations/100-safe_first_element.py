8edac7830df0fce27c8b77762b99011f6fdc05c3
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-07-11 19:33:08 +0000

    fix code error

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
 # The types of the elements of the input are not know
 from typing import Any, List, Optional
 
+
 def safe_first_element(lst: List[Any]) -> Optional[Any]:
+    """
+    Returns the first element of a list if it exists, otherwise None.
+    """
     if lst:
         return lst[0]
     else:
